a7bcaad822121070d4631b76b9b370eec29b2694
Kavish Rizvi <rizvikavish205.com>
2022-04-04 22:39:17 +0530

    Initial Commit

---
+++ src/main/java/com/credentials/apims/ApiMsApplication.java
@@ -0,0 +1,13 @@
+package com.credentials.apims;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiMsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiMsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/credentials/apims/ApiMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.credentials.apims;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiMsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
